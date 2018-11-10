ad36ca65238222f2c499d8a75474b0afa9c80739
arjuna14 <arjuna@mhs.pelitabangsa.ac.id>
2018-11-11 01:05:12 +0700

    lthn1

---
+++ latihan1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    cout<< " ______________________________________________________________"<<endl;
+    cout<< "|Algoritma Mencari Nilai Terbesar Dari Bilangan Yang Diinputkan|"<<endl;
+    cout<< "|______________________________________________________________|"<<endl;
+    int max,A;
+    max=0;
+
+    do{
+        cout<< "masukin nilai yang ingin di masukan :"; cin >> A;
+        if (max<A)
+            max=A;
+    }
+    while (A!=0);
+    cout<< "hasilnya = " <<max<< endl;
+    return 0;
+    cout<< "Terima Kasih"<<endl;
+}
